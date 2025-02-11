2cf60456819b8b49c74560dadac8990a2eede397
Shilpi <shilpitripathi947@gmail.com>
2025-02-11 10:20:10 +0530

    first qestion

---
+++ Swap Two Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int temp;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("a = %d\n",a);
+    printf("b = %d",b);
+    
+}
